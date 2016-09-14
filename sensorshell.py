665a12138e79ab52b3c00d9c13159c32aed19f19
ElD4n1 <=>
2016-09-14 10:13:06 +0000

    added main program which is just a shell currently

---
+++ sensorshell.py
@@ -0,0 +1,39 @@
+import cmd, sys
+import cv2
+
+import constants
+import capture
+import train
+from recognize import Recognizer
+
+class SensorShell(cmd.Cmd):
+	intro = 'Hello, this is the sensor shell. Type ? or help to get a list of commands.\n'
+	prompt = '$ '
+	file = None
+	
+	def do_capture(self, arg):
+		'Syntax: capture Name\nCapture a person with the specified name. (must be unique!)'
+		if arg != '':
+			capture.capture(arg)
+		else:
+			print("Syntax: capture Name")
+
+	def do_train(self, arg):
+		'Syntax: train\nTrain the model with all captured persons'
+		train.train()
+
+	def do_recognize(self, arg):
+		'Syntax: recognize\nStart the recognize loop and try periodically to recongize all persons in the image'
+		print("starting recognize loop, press 'C' to cancel")
+		recognizer = Recognizer()
+		key = 0
+		while key != 'c' and key != 'C':
+			recognizer.recognize()
+			key = cv2.waitKey(100)
+	
+	def do_exit(self, arg):
+		'Exits the shell'
+		sys.exit()
+
+if __name__ == '__main__':
+    SensorShell().cmdloop()
