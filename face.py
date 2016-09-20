565c001a14002344d104a23807d06985175bcab3
Daniel Hofer <daniel_hofer_1@hotmail.com>
2016-09-20 20:31:03 +0200

    trying another video stream solution

--- face.py
+++ face.py
@@ -1,6 +1,8 @@
 import cv2
 import threading
-from imutils.video import VideoStream
+#from imutils.video import VideoStream
+import picamera
+import picamera.array
 import imutils
 import time
 
@@ -10,24 +12,33 @@ def detect_faces(image):
 	faces = haar_cascade.detectMultiScale(image)
 	return faces
 
+with picamera.PiCamera() as camera:
+    with picamera.array.PiRGBArray(camera) as stream:
+        camera.resolution = (3240, 2464)
+		
 class FaceVideoStreamFrame(threading.Thread):
 	def __init__(self):
 		threading.Thread.__init__(self)
-		self.vs = VideoStream(usePiCamera=True, resolution=(3240, 2464))
+		#self.vs = VideoStream(usePiCamera=True, resolution=(3240, 2464))
 		self.isRunning = False
+		self.currentFrame = stream.array
 		
 	def run(self):
 		# initialize the video stream and allow the cammera sensor to warmup
-		self.vs.start()
+		#self.vs.start()
 		time.sleep(2.0)
 		self.isRunning = True
 		# loop over the frames from the video stream
 		while self.isRunning:
 			# grab the frame from the threaded video stream and resize it
 			# to have a maximum width of 400 pixels
-			frame = self.vs.read()
-			frame = imutils.resize(frame, width=400)
-
+			#frame = self.vs.read()
+			#frame = imutils.resize(frame, width=400)
+			
+			camera.capture(stream, 'bgr', use_video_port=True)
+			frame = stream.array
+			self.currentFrame = frame
+			
 			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 			faces = detect_faces(gray)
@@ -38,16 +49,20 @@ class FaceVideoStreamFrame(threading.Thread):
 
 			# show the frame
 			cv2.imshow("Frame", frame)
+			
+			stream.truncate(0) #Must use this to eliminate the error: "Incorrect buffer length"
+			
 			key = cv2.waitKey(10) & 0xFF
 		cv2.destroyAllWindows()
 
 	def stop(self):
 		self.isRunning = False
-		self.vs.stop()
+		#self.vs.stop()
 		print("VideoStream stopped, all resources freed")
 
 	def getCurrentFrame(self):
 		if(not self.isRunning):
 			time.sleep(2.1)
 		if(self.isRunning):
-			return self.vs.read()
+			#return self.vs.read()
+			return self.currentFrame
